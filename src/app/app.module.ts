135745f598922f401ceae063ca869ca76efc2d15
Julio Pérez <juliolpj@hotmail.com>
2019-03-04 18:52:49 -0300

    Navbar and hero components prueba inicial para probar publicar la página

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
