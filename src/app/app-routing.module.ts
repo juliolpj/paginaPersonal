194f9b97dfa8ab53ce940702ec6534d8b3a7d2bd
Julio Pérez <juliolpj@hotmail.com>
2019-03-05 14:27:24 -0300

    Routing y componentes(solo la estructura) para las opciones de navegación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BitacoraComponent } from './components/bitacora/bitacora.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeroComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'portafolio', component: PortafolioComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'bitacora', component: BitacoraComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BitacoraComponent } from './components/bitacora/bitacora.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeroComponent
+    HeroComponent,
+    AboutComponent,
+    PortafolioComponent,
+    BlogComponent,
+    BitacoraComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
